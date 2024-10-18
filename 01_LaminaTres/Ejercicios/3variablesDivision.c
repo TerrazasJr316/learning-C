8e05f2d11145396acc50b00442e0dd3bfcdc3f76
Josue Terrazas Mendoza <terrazasjosue0@gmail.com>
2024-10-17 22:24:28 -0600

    setting branch

--- 01_LaminaTres/Ejercicios/3variablesDivision.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-int main ( ) {
-    double e = 2.718;
-    double pi = 3.1416;
-    double div = e / pi;
-    
-    printf ("El resultado de dividir %f / %f es de = %f \n", e, pi, div);
-}
